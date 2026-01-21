096463046e4909d7897aaf618f61682aca581bcc
Salman Niazi <salman@logicalclocks.com>
2026-01-21 14:08:51 +0100

    HOPSFS-326: Release 3.2.0.18-EE-RC1

--- internal/hopsfsmount/Version.go
+++ internal/hopsfsmount/Version.go
@@ -5,7 +5,7 @@ package hopsfsmount
 var (
 	// Add Version tag, manually update
 	// Note: redeploy bin to repo.hops.works
-	VERSION = "3.2.0.18-EE-SNAPSHOT"
+	VERSION = "3.2.0.18-EE-RC1"
 
 	// GITCommit overwritten automatically by the build
 	GITCOMMIT = "HEAD"
