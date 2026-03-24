b14712e2d48fb1854033415a05cce30cb1ee5f6c
Salman Niazi <salman@logicalclocks.com>
2026-03-24 13:49:22 +0100

    HOPSFS-340: Version 3.2.0.20-EE-SNAPSHOT

--- internal/hopsfsmount/Version.go
+++ internal/hopsfsmount/Version.go
@@ -5,7 +5,7 @@ package hopsfsmount
 var (
 	// Add Version tag, manually update
 	// Note: redeploy bin to repo.hops.works
-	VERSION = "3.2.0.19-EE-SNAPSHOT"
+	VERSION = "3.2.0.20-EE-SNAPSHOT"
 
 	// GITCommit overwritten automatically by the build
 	GITCOMMIT = "HEAD"
