9783741cb77b92ca4654fe54d06d30a85fc6b3bb
Hamid Ghasemirahni <56080980+hamidgh09@users.noreply.github.com>
2026-07-01 16:15:47 +0200

    [HOPSFS-378] Bump HopsFS version to 3.4.3.3-EE-SNAPSHOT (#127)

--- internal/hopsfsmount/Version.go
+++ internal/hopsfsmount/Version.go
@@ -5,7 +5,7 @@ package hopsfsmount
 var (
 	// Add Version tag, manually update
 	// Note: redeploy bin to repo.hops.works
-	VERSION = "3.4.3.2-EE-SANPSHOT"
+	VERSION = "3.4.3.3-EE-SANPSHOT"
 
 	// GITCommit overwritten automatically by the build
 	GITCOMMIT = "HEAD"
